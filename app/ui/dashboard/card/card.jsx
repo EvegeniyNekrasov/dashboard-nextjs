5d7ae64356302ac6c17ea40a4693fdc41aad39fc
Evegeniy Nekrasov <eanekrasovart@gmail.com>
2024-03-13 20:35:59 +0100

    creating users page and products page

--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -1,5 +1,18 @@
+import { MdSupervisedUserCircle } from 'react-icons/md'
 import styles from './card.module.css'
 
 export default function Card() {
-    return <div className={styles.container}>card component</div>
+    return (
+        <div className={styles.container}>
+            <MdSupervisedUserCircle size={24} />
+            <div className={styles.texts}>
+                <span className={styles.title}>Total users</span>
+                <span className={styles.number}>10.000</span>
+                <span className={styles.detail}>
+                    <span className={styles.positive}>10%</span>
+                    more than previos week
+                </span>
+            </div>
+        </div>
+    )
 }
--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,5 +1,37 @@
+import Image from 'next/image'
 import styles from './rightbar.module.css'
-
+import { MdPlayCircleFilled } from 'react-icons/md'
 export default function Rightbar() {
-    return <div className={styles.container}>Rightbar component</div>
+    return (
+        <div className={styles.container}>
+            <div className={styles.item}>
+                <div className={styles.bgContainer}>
+                    <Image
+                        src="/astronaut.png"
+                        alt=""
+                        fill
+                        className={styles.bg}
+                    />
+                </div>
+                <div className={styles.texts}>
+                    <span className={styles.notifications}>Awailable now</span>
+                    <h3 className={styles.title}>
+                        How to use the new version of the admin dashboard
+                    </h3>
+                    <span className={styles.subtitle}>
+                        Takes 4 min to learn
+                    </span>
+                    <p className={styles.description}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua.
+                    </p>
+                    <button className={styles.button}>
+                        <MdPlayCircleFilled />
+                        Watch
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
 }
